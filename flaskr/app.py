ca46ce09f592ab4ee4cdb3cd3ea84a9fa2fabaac
MargaritaForex <ingerika.forero@gmail.com>
2024-08-10 17:48:16 -0500

    Video 4 sealizacion con marshmellow

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,6 @@
 from flaskr import create_app
 from .modelos import db, Cancion, Usuario, Album, Medio
+from .modelos import AlbumSchema
 
 app = create_app('default')
 app_context = app.app_context()
@@ -9,27 +10,33 @@ app_context.push()
 db.init_app(app)
 db.create_all()
 
-
 with app.app_context():
-    u = Usuario(nombre='Juan', contrasena='123456')
-    a = Album(titulo='prueba', anio=1999, descripcion='texto', medio=Medio.CD)
-    c = Cancion(titulo='mi cancion', minutos=1, segundos=15, interprete = 'Juanito')
-    u.albumes.append(a)
-    a.canciones.append(c)
-    db.session.add(u)
-    db.session.add(c)
+    album_schema = AlbumSchema()
+    A = Album(titulo='Prueba', anio=1999, descripcion='Texto', medio=Medio.CD)
+    db.session.add(A)
     db.session.commit()
-    #print(Usuario.query.all())
-    #print(Usuario.query.all()[0].albumes)
-    print(Album.query.all())
-    print(Album.query.all()[0].canciones)
-    print(Cancion.query.all())
-    db.session.delete(a)
-    #db.session.delete(u)
-    #print(Usuario.query.all())
-    #print(Album.query.all())
-    print(Album.query.all())
-    print(Cancion.query.all())
+    print([album_schema.dumps(album) for album in Album.query.all()])
+
+# with app.app_context():
+#     u = Usuario(nombre='Juan', contrasena='123456')
+#     a = Album(titulo='prueba', anio=1999, descripcion='texto', medio=Medio.CD)
+#     c = Cancion(titulo='mi cancion', minutos=1, segundos=15, interprete = 'Juanito')
+#     u.albumes.append(a)
+#     a.canciones.append(c)
+#     db.session.add(u)
+#     db.session.add(c)
+#     db.session.commit()
+#     #print(Usuario.query.all())
+#     #print(Usuario.query.all()[0].albumes)
+#     print(Album.query.all())
+#     print(Album.query.all()[0].canciones)
+#     print(Cancion.query.all())
+#     db.session.delete(a)
+#     #db.session.delete(u)
+#     #print(Usuario.query.all())
+#     #print(Album.query.all())
+#     print(Album.query.all())
+#     print(Cancion.query.all())
 
 
 #prueba
